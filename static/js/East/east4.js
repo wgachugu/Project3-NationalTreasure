76857530d61a8e5e69bf5a81d6a88b6cd4c77dfa
Wilson <ericowilson87@gmail.com>
2023-01-15 18:07:37 -0500

    html script updates with visualizations included

---
+++ static/js/East/east4.js
@@ -0,0 +1,10 @@
+// Getting a reference to the button on the page with the id property set to `east1`
+var button = d3.select("#east4");
+
+button.on("click", function() {
+    // d3.select(".east1-img").html("<img src='{{url_for('static', filename='images/East_Region/east1_Great_smoky_mountains.JPG')}}' alt='Great Smoky Mountains'>");
+    d3.select(".east-img").html('')
+    d3.select(".east-img").html("<img src='../static/images/East_Region/east4_Shenandoah.jpg' alt='Shenandoah'>");     
+  });
+
+  // clear html for next button click
