0150d415454125ecfb5715f496191072946f06bd
Wilson <ericowilson87@gmail.com>
2023-01-16 18:25:45 -0500

    including central images

--- static/js/Central/central2.js
+++ static/js/Central/central2.js
@@ -4,7 +4,7 @@ var button = d3.select("#central2");
 button.on("click", function() {
     // d3.select(".east1-img").html("<img src='{{url_for('static', filename='images/East_Region/east1_Great_smoky_mountains.JPG')}}' alt='Great Smoky Mountains'>");
     d3.select(".central-img").html('')
-    d3.select(".central-img").html("<img src='../static/images/Central/###.jpeg' alt='Indiana Dunes'>");     
+    d3.select(".central-img").html("<img src='../static/images/Central/central_indianadunes_two.jpg' alt='Indiana Dunes'>");     
   });
 
   // clear html for next button click
--- static/js/Central/central5.js
+++ static/js/Central/central5.js
@@ -4,7 +4,7 @@ var button = d3.select("#central5");
 button.on("click", function() {
     // d3.select(".east1-img").html("<img src='{{url_for('static', filename='images/East_Region/east1_Great_smoky_mountains.JPG')}}' alt='Great Smoky Mountains'>");
     d3.select(".central-img").html('')
-    d3.select(".central-img").html("<img src='../static/images/Central/###.jpeg' alt='Badlands'>");     
+    d3.select(".central-img").html("<img src='../static/images/Central/central_badlands_five.jpg' alt='Badlands'>");     
   });
 
   // clear html for next button click
