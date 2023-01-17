be561e6dadf5664fb1f6063d048223537c2eca04
Mathi2702 <elamathi.0409@gmail.com>
2023-01-16 20:53:33 -0700

    updated freeze.py

--- freeze.py
+++ freeze.py
@@ -3,6 +3,5 @@ from app import app
 from config import base_url
 
 if __name__ == '__main__':
-    app.config["FREEZER_BASE_URL"] = https://wgachugu/Project3-NationalTreasure
-    freezer = Freezer(app)
+    app.config["FREEZER_BASE_URL"] = "https://github.com/wgachugu/Project3-NationalTreasure"
     freezer.freeze()
