7545172a8a579c66588889d2dcbc8324b06615be
Mathi2702 <elamathi.0409@gmail.com>
2023-01-17 18:47:16 -0700

    Deploy to Github Pages

--- static/js/Central/central4.js
+++ static/js/Central/central4.js
@@ -4,7 +4,7 @@ var button = d3.select("#central4");
 button.on("click", function() {
     // d3.select(".east1-img").html("<img src='{{url_for('static', filename='images/East_Region/east1_Great_smoky_mountains.JPG')}}' alt='Great Smoky Mountains'>");
     d3.select(".central-img").html('')
-    d3.select(".central-img").html("<img src='../static/images/Central/###.jpeg' alt='Hot Springs'>");     
+    d3.select(".central-img").html("<img src='../static/images/Central/central_hotsprings_three.jpg' alt='Hot Springs'>");     
   });
 
   // clear html for next button click
