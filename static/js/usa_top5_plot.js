c9490867ccea6f854e3fb6e3ec6bf545e5ae4f74
Wilson <ericowilson87@gmail.com>
2023-01-17 21:38:33 -0500

    another update

--- static/js/usa_top5_plot.js
+++ static/js/usa_top5_plot.js
@@ -7,14 +7,14 @@ fetch("Project3-NationalTreasure/api/kaggle.json")
   })
   .then(data => {
     // Sort data by visitors(2021) in descending order
-    data.sort((a, b) => b["Recreation visitors (2021)[11]"] - a["Recreation visitors (2021)[11]"]);
+    data.sort((a, b) => b["visitors(2021)"] - a["visitors(2021)"]);
 
     // Get the top 5 most visited sites
     const topFive = data.slice(0, 5);
 
     // Extract the names and visitor counts of the top five sites
     const siteNames = topFive.map(site => site.Name);
-    const siteVisitors = topFive.map(site => site["Recreation visitors (2021)[11]"]);
+    const siteVisitors = topFive.map(site => site["visitors(2021)"]);
 
     // Use Plotly.js to create the bar chart
     const plotDiv = document.getElementById("plot");
